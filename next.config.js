00338a09080108989cdd4b943bf602e142551ea3
JamesCroissant <yuham1014@gmail.com>
2024-03-03 22:40:41 -0800

    add auth function

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import(‘next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
+  // experimental: {
+  //   serverActions: true,
+  // },
   images: {
     domains: ['lh3.googleusercontent.com', 'res.cloudinary.com', 'spoonacular.com'],
   },
